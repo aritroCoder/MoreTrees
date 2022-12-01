9e7886f2b5fbd4224e73d47e30181d252e017a6a
Malcolm <malcolmharrellmedia@gmail.com>
2022-11-30 17:53:30 -0800

    added style below hero section

--- pages/index.js
+++ pages/index.js
@@ -40,8 +40,8 @@ export default function Home() {
       </div>
       <span className={styles.herotext}>Plant more trees.</span>
       <div className={styles.main}>
-        <h1>Welcome, {username}</h1>
-        {loggedIn && <p>You have planted {JSON.parse(localStorage.getItem('user')).plantCount} trees so far{JSON.parse(localStorage.getItem('user')).plantCount>1?' 🥳':', plant some more!'}</p>}
+        <h1 className={styles.welcomeH1}>Welcome, {username}</h1>
+        {loggedIn && <p className={styles.displayCount}>You have planted {JSON.parse(localStorage.getItem('user')).plantCount} trees so far{JSON.parse(localStorage.getItem('user')).plantCount>1?' 🥳':', plant some more!'}</p>}
         {loggedIn && <div className={styles.centered}><button className={styles.plantNow} onClick={plantNowHandler}>Plant a tree!</button></div>}
         {loggedIn && <div className={styles.logout}><button className={styles.logout} onClick={logOutHandler}>Log Out</button></div>}
       </div>
