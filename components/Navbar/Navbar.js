61a6429dfdd165bb21ed7692d557370a0eb9cbfd
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-29 13:47:18 +0530

    navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -45,22 +45,19 @@ const Navbar = () => {
             <div class="offcanvas-body">
               <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
+                  <a class="nav-link" href="/about">About</a>
                 </li>
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Dropdown
                   </a>
                   <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="/signup">Signup</a></li>
+                    <li><a class="dropdown-item" href="/login">Login</a></li>
+                   
                   </ul>
                 </li>
               </ul>
