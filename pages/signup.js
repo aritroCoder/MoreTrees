92dd121f54e54c00bdf3f5da3e587889f4894179
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-30 00:42:39 +0530

    fixed

--- pages/signup.js
+++ pages/signup.js
@@ -47,6 +47,7 @@ const signup = () => {
         <>
            <Head>
            <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.5"/>
+           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
 
           </Head>
 
