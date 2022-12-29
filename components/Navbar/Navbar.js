02d8310ab2573a25da7aba9ad6848c4d8dd95c56
Tushar98644 <tusharbanik@Tushars-MacBook-Air.local>
2022-12-29 13:41:55 +0530

    navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -29,27 +29,49 @@ const Navbar = () => {
         <Link href="/signup" ><a className={styles.norm}>Sign Up</a></Link>
 
 
-        <div >
-          <a class={styles.icon} onClick={()=>{
-            setshow(!show)
-          }}>
-            <i class="fa fa-bars menu"></i>
-          </a>
+
+      </div>
+      <nav class={`${styles.main} navbar bg-body-tertiary fixed-top`}>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                      <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="d-flex mt-3" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
         </div>
-
-      </div>
-      <div class={styles.topnav}>
-      {show && 
-        <><a href="/signup" class={styles.active}>Signup</a><div id="myLinks">
-            <a href="/login">Login</a>
-            <a href="/contact">Contact</a>
-            <a href="/about">About</a>
-          </div></>
-        }
-
-
-
-      </div>
+      </nav>
 
     </>
   )
